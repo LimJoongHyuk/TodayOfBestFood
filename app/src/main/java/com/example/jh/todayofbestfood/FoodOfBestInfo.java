85f1c791b8a32c4ed2ac1664d663879d32a2a0a6
임중혁 <chimi2802@naver.com>
2017-05-22 16:39:27 +0900

    food 클래스 추가, 맵 중간작업

---
+++ app/src/main/java/com/example/jh/todayofbestfood/FoodOfBestInfo.java
@@ -0,0 +1,34 @@
+package com.example.jh.todayofbestfood;
+
+/**
+ * Created by jh on 2017-05-22.
+ */
+
+public class FoodOfBestInfo {
+    private String restaurant_name;
+    private String restaurant_address;
+    private String restaurant_recommend_food;
+    private float restaurant_grade;
+    private String food_postscript;
+
+
+    public String getRestaurant_name() {
+        return restaurant_name;
+    }
+
+    public String getRestaurant_address() {
+        return restaurant_address;
+    }
+
+    public String getRestaurant_recommend_food() {
+        return restaurant_recommend_food;
+    }
+
+    public float getRestaurant_grade() {
+        return restaurant_grade;
+    }
+
+    public String getFood_postscript() {
+        return food_postscript;
+    }
+}
--- app/src/main/java/com/example/jh/todayofbestfood/MainActivity.java
+++ app/src/main/java/com/example/jh/todayofbestfood/MainActivity.java
@@ -2,14 +2,39 @@ package com.example.jh.todayofbestfood;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.jh.todayofbestfood.R.id.btnAdd;
+import static com.example.jh.todayofbestfood.R.id.btnSelect;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_select;
+    private Button button_dataInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button_select = (Button) findViewById(btnSelect);
+        button_dataInput = (Button) findViewById(btnAdd);
 
     }
+
+    private View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            switch ( v.getId() ) {
+                case btnSelect:
+
+                    break;
+
+                case btnAdd:
+
+                    break;
+            }
+        }
+    };
 }

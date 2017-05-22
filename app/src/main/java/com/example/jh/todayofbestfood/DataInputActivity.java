dcd86739e9a5eaef448f993cf2bf50a4d53a0a24
임중혁 <chimi2802@naver.com>
2017-05-22 14:51:52 +0900

    datainput 엑티비티 작업

---
+++ app/src/main/java/com/example/jh/todayofbestfood/DataInputActivity.java
@@ -0,0 +1,19 @@
+package com.example.jh.todayofbestfood;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jh on 2017-05-22.
+ */
+
+public class DataInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datainput);
+
+
+    }
+}

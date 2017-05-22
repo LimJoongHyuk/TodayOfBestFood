b8a00ed8f52602f4a7b2300b2bb7ba3d86de49a0
pdg <zja1vb2xj3@naver.com>
2017-05-22 16:34:05 +0900

    #ReviewInputFragment.java 추가 #fragment_reviewinput.xml 추가 #fragment_reviewoutput.xml 추가

---
+++ app/src/main/java/com/example/jh/todayofbestfood/ReviewInputFragment.java
@@ -0,0 +1,17 @@
+package com.example.jh.todayofbestfood;
+
+import android.app.Fragment;
+
+/**
+ * Created by pdg on 2017-05-22.
+ */
+
+public class ReviewInputFragment extends Fragment {
+
+
+
+
+    interface ReviewClickListener{
+        void onClick(int position);
+    }
+}

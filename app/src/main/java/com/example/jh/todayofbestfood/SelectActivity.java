07f50c5c2921990e1ec19b70a9eed6679198a4eb
임중혁 <chimi2802@naver.com>
2017-05-23 10:02:02 +0900

    map 구글 계정등록중

--- app/src/main/java/com/example/jh/todayofbestfood/SelectActivity.java
+++ app/src/main/java/com/example/jh/todayofbestfood/SelectActivity.java
@@ -1,13 +1,17 @@
 package com.example.jh.todayofbestfood;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SelectActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+
+
     }
 }

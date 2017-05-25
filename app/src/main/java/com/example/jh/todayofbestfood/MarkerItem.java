32f102ef503f786d79dcbe1f14837b8ab0bf32eb
smh <smh0660@gmail.com>
2017-05-25 16:11:37 +0900

    selectActivity 음식점 marker 구현

---
+++ app/src/main/java/com/example/jh/todayofbestfood/MarkerItem.java
@@ -0,0 +1,92 @@
+package com.example.jh.todayofbestfood;
+
+/**
+ * Created by smh on 2017-05-25.
+ */
+
+public class MarkerItem {
+    private int restaurantId;
+    private String restaurantName;
+    private String foodTag;
+    private String restaurantRecommendFood;
+    private float restaurantGrade;
+    private Double restaurantLatitude;
+    private Double restaurantLongitude;
+    private String foodImageName;
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getFoodTag() {
+        return foodTag;
+    }
+
+    public void setFoodTag(String foodTag) {
+        this.foodTag = foodTag;
+    }
+
+    public String getRestaurantRecommendFood() {
+        return restaurantRecommendFood;
+    }
+
+    public void setRestaurantRecommendFood(String restaurantRecommendFood) {
+        this.restaurantRecommendFood = restaurantRecommendFood;
+    }
+
+    public float getRestaurantGrade() {
+        return restaurantGrade;
+    }
+
+    public void setRestaurantGrade(float restaurantGrade) {
+        this.restaurantGrade = restaurantGrade;
+    }
+
+    public double getRestaurantLatitude() {
+        return restaurantLatitude;
+    }
+
+    public void setRestaurantLatitude(Double restaurantLatitude) {
+        this.restaurantLatitude = restaurantLatitude;
+    }
+
+    public double getRestaurantLongitude() {
+        return restaurantLongitude;
+    }
+
+    public void setRestaurantLongitude(Double restaurantLongitude) {
+        this.restaurantLongitude = restaurantLongitude;
+    }
+
+    public String getFoodImageName() {
+        return foodImageName;
+    }
+
+    public void setFoodImageName(String foodImageName) {
+        this.foodImageName = foodImageName;
+    }
+
+    public MarkerItem(int restaurantId, String restaurantName, String foodTag, String restaurantRecommendFood
+            , float restaurantGrade, Double restaurantLatitude, Double restaurantLongitude, String foodImageName) {
+        this.restaurantId = restaurantId;
+        this.restaurantName = restaurantName;
+        this.foodTag = foodTag;
+        this.restaurantRecommendFood = restaurantRecommendFood;
+        this.restaurantGrade = restaurantGrade;
+        this.restaurantLatitude = restaurantLatitude;
+        this.restaurantLongitude = restaurantLongitude;
+        this.foodImageName = foodImageName;
+    }
+}
--- app/src/main/java/com/example/jh/todayofbestfood/SelectActivity.java
+++ app/src/main/java/com/example/jh/todayofbestfood/SelectActivity.java
@@ -1,39 +1,66 @@
 package com.example.jh.todayofbestfood;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SelectActivity extends AppCompatActivity {
-    private static final String TESTPATH = "/storage/emulated/0/DCIM/Camera/20170524_112204.jpg";
+import java.util.ArrayList;
+
+public class SelectActivity extends AppCompatActivity implements GoogleMap.OnMarkerClickListener{
+    private static final String PATH = "/storage/emulated/0/DCIM/Camera/";
     private static final String TAG = "SelectActivity";
+
+    TextView txtRestaurantName, txtRestaurantTag, txtRecommendFood;
+    RatingBar ratingBar;
+    ImageView imgSelect;
+
+    View markerView;
+    ImageView markerImage;
 
     SupportMapFragment mapFragment;
     GoogleMap map;
     private MarkerOptions _markerOptions;
     static final LatLng SEOUL = new LatLng(37.56, 126.97);
     private ImageView imageView_food;
+    private ArrayList<MarkerItem> _markerItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
-        imageView_food = (ImageView) findViewById(R.id.imageView_food);
+
+        imgSelect = (ImageView) findViewById(R.id.imageView_inselect);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar_inselect);
+        txtRestaurantName = (TextView)findViewById(R.id.textView_restaurantName);
+        txtRestaurantTag = (TextView)findViewById(R.id.textView_tag);
+        txtRecommendFood = (TextView)findViewById(R.id.textView_RecommendFood);
+
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
+
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
@@ -41,11 +68,14 @@ public class SelectActivity extends AppCompatActivity {
 
                 map = googleMap;
 
+                setCustomMarkerView();
+                getMarkerItems();
+
                 _markerOptions = new MarkerOptions();
                 _markerOptions.position(SEOUL);
                 map.addMarker(_markerOptions);
-                map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-                map.animateCamera(CameraUpdateFactory.zoomTo(15));
+                zoomCamera(SEOUL);
+                map.setOnMarkerClickListener((GoogleMap.OnMarkerClickListener) SelectActivity.this);
             }
         });
 
@@ -63,6 +93,11 @@ public class SelectActivity extends AppCompatActivity {
 
     }
 
+    private void zoomCamera(LatLng latLng) {
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        map.animateCamera(CameraUpdateFactory.zoomTo(18));
+    }
+
     private void reviewButtonClick(View view) {
         Intent intent = new Intent(getApplicationContext(), ReviewActivity.class);
         startActivity(intent);
@@ -70,7 +105,6 @@ public class SelectActivity extends AppCompatActivity {
 
     private void locationButtonClick(View view) {
         requestMyLocation();
-        setImage();
     }
 
     private void requestMyLocation() {
@@ -92,15 +126,98 @@ public class SelectActivity extends AppCompatActivity {
 
         map.clear();
         map.addMarker(_markerOptions);
+        setCustomMarkerView();
+        getMarkerItems();
 
-        map.moveCamera(CameraUpdateFactory.newLatLng(curPoint));
-        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+        zoomCamera(curPoint);
     }
 
-    private void setImage() {
+
+
+    private void setCustomMarkerView() {
+        markerView = LayoutInflater.from(this).inflate(R.layout.marker_layout, null);
+        markerImage = (ImageView) markerView.findViewById(R.id.markerImage);
+    }
+
+    private void getMarkerItems() {
+        _markerItems = new ArrayList<>();
+
+        //데이터 받아오기
+
+        _markerItems.add(new MarkerItem(1, "삼국지", "중식", "볶음밥", (float)4.5, 37.542425, 126.841514, "dd"));
+        _markerItems.add(new MarkerItem(2, "신선설농탕", "한식", "설렁탕", (float) 3.5, 37.540722, 126.838335, "dd"));
+        _markerItems.add(new MarkerItem(3, "SUBWAY", "패스트푸드", "샌드위치", (float) 1.5, 37.540948, 126.840623, "dd"));
+
+        for(MarkerItem markerItem : _markerItems) {
+            addMarker(markerItem);
+        }
+    }
+
+    private void addMarker(MarkerItem markerItem) {
+        LatLng position = new LatLng(markerItem.getRestaurantLatitude(), markerItem.getRestaurantLongitude());
+
+        String restaurantName= markerItem.getRestaurantName();
+        String food_tag = markerItem.getFoodTag();
+
+        _markerOptions = new MarkerOptions();
+        _markerOptions.title(restaurantName);
+        _markerOptions.position(position);
+
+        switch (food_tag) {
+            case "중식" :
+                markerImage.setImageResource(R.drawable.chinesefood_icon_off);
+                break;
+            case "패스트푸드" :
+                markerImage.setImageResource(R.drawable.fastfood_icon);
+                break;
+            case "한식" :
+                markerImage.setImageResource(R.drawable.koreanfood_icon);
+                break;
+            case "일식" :
+                markerImage.setImageResource(R.drawable.japanesefood_icon);
+                break;
+            default:
+        }
+
+        _markerOptions.icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this, markerView)));
+        map.addMarker(_markerOptions);
+    }
+
+    private Bitmap createDrawableFromView(Context context, View view) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        for(MarkerItem markerItem : _markerItems) {
+            if(markerItem.getRestaurantName().equals(marker.getTitle())) {
+                txtRestaurantName.setText(markerItem.getRestaurantName());
+                txtRestaurantTag.setText("구분 : " + markerItem.getFoodTag());
+                txtRecommendFood.setText("추천 메뉴 : " + markerItem.getRestaurantRecommendFood());
+                ratingBar.setRating(markerItem.getRestaurantGrade());
+                setImage(markerItem.getFoodImageName());
+            }
+        }
+        LatLng latLng = new LatLng(marker.getPosition().latitude, marker.getPosition().longitude);
+        zoomCamera(latLng);
+        return true;
+    }
+
+    private void setImage(String string) {
         try {
-            Bitmap bitmap = BitmapFactory.decodeFile(TESTPATH);
-            imageView_food.setImageBitmap(bitmap);
+            Bitmap bitmap = BitmapFactory.decodeFile(PATH + string);
+            imgSelect.setImageBitmap(bitmap);
         }catch (Exception e) {
             e.printStackTrace();
         }
